a767d26b6d24901c317c91d42c402fce5639cdb4
ATHARVA JOSHI <114106490+AJJSSJJ@users.noreply.github.com>
2023-02-16 22:58:01 +0530

    removed shanghai funciton

--- script.js
+++ script.js
@@ -33,26 +33,7 @@ submit.addEventListener("click",(e)=>{
 
 getweather("Delhi")
 
-function shanghai() {
-	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Shanghai', options)
-	.then(response => response.json())
-	.then((response) => {
-		
 
-		console.log(response)
-		cloud_pct2.innerHTML = response.cloud_pct
-		temp2.innerHTML = response.temp
-		feels_like2.innerHTML = response.feels_like
-		humidity2.innerHTML = response.humidity
-		min_temp2.innerHTML = response.min_temp
-		max_temp2.innerHTML = response.max_temp
-		wind_speed2.innerHTML = response.wind_speed
-		wind_degrees2.innerHTML = response.wind_degrees
-		sunrise2.innerHTML = response.sunrise
-		sunset2.innerHTML = response.sunset
-	})
-	.catch(err => console.error(err));
-}
 
 	
 
